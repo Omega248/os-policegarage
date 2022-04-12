66b6eb9a64dc7e2d02d45e2dce0821762c19fbf0
Omega <86800507+OmegaGrape@users.noreply.github.com>
2022-04-12 05:32:38 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.Pedlocation = {
+    {Cords = vector3(459.6, -986.6, 24.7), h = 90.88},
+}
+
+Config.NPCmodel = 'ig_trafficwarden'
+
+-- For help with changing the Vehicle Mods, Extras and Colours refer to the read me
