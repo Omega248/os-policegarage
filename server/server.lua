1ed4464853f9c88fa867555cd95355e1738eb7d1
Omega Scripts <86800507+Omega248@users.noreply.github.com>
2024-01-24 02:47:22 +0000

    Version check

---
+++ server/server.lua
@@ -0,0 +1,27 @@
+local function CheckVersion()
+    PerformHttpRequest('https://raw.githubusercontent.com/Omega248/os-policegarage/main/version', function(err, newestVersion, headers)
+        -- Check if there is an error in the HTTP request
+        if err ~= 200 then
+            print("HTTP Request Error. Error code: " .. tostring(err))
+            return
+        end
+
+        -- Check if the newest version information is available
+        if newestVersion == nil or newestVersion == "" then
+            print("Failed to retrieve the latest version. The response was empty.")
+            return
+        end
+
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version'):gsub("%s+", "")
+        newestVersion = newestVersion:gsub("%s+", "")
+
+        if newestVersion == currentVersion then
+            return
+        else 
+            print("^1You are running an outdated version of " .. GetCurrentResourceName() .. ".^7")
+            print("Current Version: " .. currentVersion .. ", Latest Version: " .. newestVersion)
+            print("Please download the latest version from: https://github.com/Omega248/os-policegarage/releases")
+        end
+    end, 'GET')
+end
+CheckVersion()
