f03d7e654fcf335b4816f67d823f36376fab7e43
Omega Scripts <86800507+Omega248@users.noreply.github.com>
2024-01-24 02:44:29 +0000

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,18 @@
 fx_version 'adamant'
 game 'gta5'
 
-
 description 'os-policegarage'
 version '2.2.0'
 author 'Omega Scripts'
-
 
 client_scripts {
     'client/main.lua',
     'client/menu.lua',
     'client/target.lua'
+}
+
+server_script {
+    'server/server.lua'
 }
 
 shared_script 'config.lua'
