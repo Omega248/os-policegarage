083b740d84a50c06219684b73ce5b5ea5ad58885
Omega Scripts <reecestangoe0824@gmail.com>
2025-02-22 19:06:09 +0000

    Update menu.lua
    - Added trunk item support via qb-inventory
    - General code cleanup & optimization
    - Updated menu to show only vehicles & departments the player can drive

--- client/menu.lua
+++ client/menu.lua
@@ -1,38 +1,52 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+-- Create a department menu, filtering out vehicles the player isn’t allowed to drive
 function CreateDepartmentMenu(departmentName, departmentVehicles)
     local menuItems = {
         {
             header = '< Go Back',
-            params = {
-                event = "garage:policemenu"
-            }
+            params = { event = "garage:policemenu" }
         }
     }
 
+    -- Get updated player data here
+    local playerData = QBCore.Functions.GetPlayerData()
+    local playerJob = (playerData and playerData.job and playerData.job.name) or nil
+    local playerRank = (playerData and playerData.job and (playerData.job.grade.level or playerData.job.grade)) or 0
+    print("DEBUG: Player Job:", playerJob, "Player Rank:", playerRank)
+
     for vehicleName, vehicleData in pairs(departmentVehicles) do
-        table.insert(menuItems, {
-            header = vehicleData.label,
-            params = {
-                event = "policegarage:spawnVehicle",
-                args = {
-                    Department = departmentName,
-                    vehicle = vehicleData,
+        local canDrive = true
+
+        if Config.RestrictVehiclesRank and vehicleData.rank and playerRank < vehicleData.rank then
+            canDrive = false
+        end
+
+        if Config.RestrictVehiclesJob and vehicleData.job and playerJob and vehicleData.job ~= playerJob then
+            canDrive = false
+        end
+
+        if canDrive then
+            table.insert(menuItems, {
+                header = vehicleData.label,
+                params = {
+                    event = "policegarage:spawnVehicle",
+                    args = {
+                        Department = departmentName,
+                        vehicle = vehicleData,
+                    }
                 }
-            }
-        })
+            })
+        end
     end
 
     return menuItems
 end
-
 RegisterNetEvent('garage:policemenu', function()
     local mainMenuItems = {
         {
             header = 'Police Garage',
-            params = {
-                event = "garage:policemenu"
-            }
+            params = { event = "garage:policemenu" }
         }
     }
 
@@ -40,37 +54,34 @@ RegisterNetEvent('garage:policemenu', function()
     for departmentName in pairs(Config.DepartmentVehicles) do
         table.insert(departmentNames, departmentName)
     end
-
     table.sort(departmentNames)
 
     for _, departmentName in ipairs(departmentNames) do
         local departmentVehicles = Config.DepartmentVehicles[departmentName]
-        table.insert(mainMenuItems, {
-            header = departmentName,
-            params = {
-                event = "qb-menu:client:openDepartmentMenu",
-                args = {
-                    departmentName = departmentName,
-                    departmentVehicles = departmentVehicles
+        -- Build the department menu to check if any vehicles are allowed
+        local departmentMenu = CreateDepartmentMenu(departmentName, departmentVehicles)
+        if #departmentMenu > 1 then  -- More than just the "< Go Back" option
+            table.insert(mainMenuItems, {
+                header = departmentName,
+                params = {
+                    event = "qb-menu:client:openDepartmentMenu",
+                    args = {
+                        departmentName = departmentName,
+                        departmentVehicles = departmentVehicles
+                    }
                 }
-            }
-        })
+            })
+        end
     end
 
     table.insert(mainMenuItems, {
         header = 'Store Vehicle',
-        params = {
-            event = "policegarage:storecar",
-            args = {}
-        }
+        params = { event = "policegarage:storecar", args = {} }
     })
 
     table.insert(mainMenuItems, {
         header = 'X Close',
-        params = {
-            event = "qb-menu:closeMenu",
-            args = {}
-        }
+        params = { event = "qb-menu:closeMenu", args = {} }
     })
 
     exports['qb-menu']:openMenu(mainMenuItems)
