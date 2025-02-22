bf86d19c454c8d6eb14a41031ff351dc6aea94a1
Omega Scripts <reecestangoe0824@gmail.com>
2025-02-22 19:08:34 +0000

    Update fxmanifest.lua
    - Added trunk item support via qb-inventory
    - General code cleanup & optimization
    - Updated menu to show only vehicles & departments the player can drive

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,11 +2,12 @@ fx_version 'adamant'
 game 'gta5'
 
 description 'os-policegarage'
-version '2.2.0'
+version '2.3.0'
 author 'Omega Scripts'
 
 client_scripts {
     'client/main.lua',
+    '@qb-core/shared/locale.lua',
     'client/menu.lua',
     'client/target.lua'
 }
